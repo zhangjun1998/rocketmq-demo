771f6fb028b95a81139315fb627d3ff4d2d246d2
ZhangJun <zhangjun_java@163.com>
2020-12-09 10:57:12 +0800

    modified:   src/main/java/com/zjcoding/demo/broadcast/BroadcastConsumer1.java

--- src/main/java/com/zjcoding/demo/broadcast/BroadcastConsumer1.java
+++ src/main/java/com/zjcoding/demo/broadcast/BroadcastConsumer1.java
@@ -4,6 +4,7 @@ import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.MessageExt;
 import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
@@ -21,7 +22,10 @@ public class BroadcastConsumer1 {
         DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("1consumer");
         consumer.setNamesrvAddr("localhost:9876");
         consumer.setConsumerGroup("CON");
-        // consumer.setMessageModel(MessageModel.BROADCASTING);
+        // 设置广播模式消费
+        consumer.setMessageModel(MessageModel.BROADCASTING);
+        // 设置消费起始点
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
         consumer.subscribe("TopicB","*");
 
         consumer.registerMessageListener(new MessageListenerConcurrently() {
